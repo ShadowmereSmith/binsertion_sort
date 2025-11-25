da21ac50889b30720e1ff0bf4d54b0b4698727a0
Wesley Henrique Ferreira <wes04hf@gmail.com>
2025-11-25 19:29:11 -0300

    Update binsertion_sort.v

--- binsertion_sort.v
+++ binsertion_sort.v
@@ -38,7 +38,7 @@ Proof.
     + simpl length. apply Nat.lt_le_incl. apply Nat.div_lt.
       * lia.
       * auto.
-  - intros. rewrite <- teq1. rewrite length_skipn. simpl length. apply Nat.sub_lt.
+  - intros. rewrite <- teq1. rewrite skipn_length. simpl length. apply Nat.sub_lt.
     + apply Nat.lt_le_incl. apply Nat.div_lt.
       * lia.
       * auto.
@@ -148,7 +148,7 @@ Proof.
   - intros. rewrite firstn_length_le.
     + simpl length. apply Nat.div_lt; lia.
     + simpl length. apply Nat.lt_le_incl. apply Nat.div_lt; lia.
-  - intros. rewrite <- teq1. rewrite length_skipn. apply Nat.sub_lt.
+  - intros. rewrite <- teq1. rewrite skipn_length. apply Nat.sub_lt.
     + simpl length. apply Nat.lt_le_incl. apply Nat.div_lt; lia.
     + simpl length. apply Nat.div_str_pos. lia.
 Defined.
