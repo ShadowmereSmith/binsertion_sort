e2a0e60b0d56a756a2726be26cc16b5ad0365042
Geilson Sá <geilsondossantos098@gmail.com>
2025-11-26 23:35:09 -0300

    Proposta de projeto

--- binsertion_sort.v
+++ binsertion_sort.v
@@ -1,198 +1,167 @@
-Require Import List.
-Require Import Arith.
-Require Import Lia.
-Import ListNotations.
+(** PROJETO E ANÁLISE DE ALGORITMOS - 2025/2 *)
+(** Link do site do coq se vocês prefirirem: https://jscoq.github.io/scratchpad.html
 
-(**
-   A função [bsearch x l] retorna a posição [i] ($0 \leq i \leq |l|-1$) onde [x] deve ser inserido na lista ordenada [l]:
- *)
-
-Require Import Recdef.
-
-Function bsearch x l {measure length l} :=
-  match l with
-  | [] => 0
-  | [y] => if (x <=? y)
-           then 0
-           else 1
-  | h1::h2::tl =>
-      let len := length l in
-      let mid := len / 2 in
-      let l1 := firstn mid l in
-      let l2 := skipn mid l in
-      match l2 with
-      | [] => 0
-      | h2'::l2' => if (x <? h2')
-                    then bsearch x l1
-                    else
-                      if x =? h2'
-                      then mid
-                      else mid + (bsearch x l2)
-      end
-  end.
-Proof.
-  - intros. rewrite firstn_length_le.
-    + simpl length. apply Nat.div_lt.
-      * lia.
-      * auto.
-    + simpl length. apply Nat.lt_le_incl. apply Nat.div_lt.
-      * lia.
-      * auto.
-  - intros. rewrite <- teq1. rewrite skipn_length. simpl length. apply Nat.sub_lt.
-    + apply Nat.lt_le_incl. apply Nat.div_lt.
-      * lia.
-      * auto.
-    + apply Nat.div_str_pos. lia.
-Defined.
-
-(**
-   Podemos fazer alguns testes com esta função:
- *)
-
-Lemma test0: bsearch 1 [] = 0.
-Proof.
-  rewrite bsearch_equation. reflexivity.
-Qed.
-
-Lemma test1: bsearch 1 [0;2;3] = 1.
-Proof.
-  rewrite bsearch_equation. simpl. rewrite bsearch_equation. destruct (1 <=? 0) eqn: H.
-  - inversion H.
-  - reflexivity.
-Qed.
-
-Lemma test2: bsearch 2 [0;2;3] = 1.
-Proof.
-  rewrite bsearch_equation. simpl. reflexivity.
-Qed.
-  
-Lemma test3: bsearch 2 [0;2;3;4] = 1.
-Proof.
-  rewrite bsearch_equation. simpl. rewrite bsearch_equation. simpl. reflexivity.
-Qed.
-
-Lemma test4: bsearch 3 [0;1;2;3;4;5] = 3.
-Proof.
-  rewrite bsearch_equation. simpl. reflexivity.
-Qed.  
-
-(**
-Também podemos verificar que [bsearch x l] sempre retorna uma posição válida da lista [l]:
- *)
-
-Lemma bsearch_valid_pos: forall l x, 0 <= bsearch x l < length l.
-Proof.
-  Admitted.
-
-  
-(**
-A seguir, definiremos a função [insert_at i x l] que insere o elemento [x] na posição [i] da lista [l]:
- *)
-
-Fixpoint insert_at i (x:nat) l :=
-  match i with
-  | 0 => x::l
-  | S k => match l with
-           | nil => [x]
-           | h::tl => h::(insert_at k x tl)
-           end
-  end.
-
-Eval compute in (insert_at 2 3 [1;2;3]).
-
-(**
-A função [binsert x l] a seguir insere o elemento x na posição retornada por [bsearch x l] de [l]:
- *)
-
-Definition binsert x l :=
-  let pos := bsearch x l in
-  insert_at pos x l.
-
-(**
- Agora podemos enunciar o teorema que caracteriza a correção da função [binsert], a saber, que [binsert x l] retorna uma lista ordenada, se [l] estiver ordenada: 
+Nome e username dos participantes:
+1. 
+2. Geilson - Geilsondss 
+3. 
 *)
-(* begin hide *)
-Require Import Sorted.
-(* end hide *)
+   
+   (** *Correção do algoritmo mergesort *)
 
-Theorem binsert_correct: forall l x, Sorted le l -> Sorted le (binsert x l).
-Proof.
-Admitted.    
+(** O algoritmo mergesort é um algoritmo de ordenação que utiliza a técnica de divisão e conquista, que consiste das seguintes etapas:
+Divisão: O algoritmo divide a lista l recebida como argumento ao meio, obtendo as listas l_1 e l_2. 
+Conquista: O algoritmo é aplicado recursivamente às listas l_1 e l_2 gerando, respectivamente, as listas ordenadas l_1' e l_2'.
+Combinação: O algoritmo combina as listas l_1' e l_2' através da função merge que então gera a saída do algoritmo.
+Por exemplo, ao receber a lista (4 :: 2 :: 1 :: 3 :: nil), este algoritmo inicialmente divide esta lista em duas sublistas, a saber (4 :: 2 :: nil) e (1 :: 3 :: nil). O algoritmo é aplicado recursivamente às duas sublistas para ordená-las, e ao final deste processo, teremos duas listas ordenadas (2 :: 4 :: nil) e (1 :: 3 :: nil). Estas listas são, então, combinadas para gerar a lista de saída (1 :: 2 :: 3 :: 4 :: nil). *))
 
-(**
-   Alternativamente, podemos construir uma única função que combina a execução de [bsearch] e [insert_at]. A função [binsert x l] a seguir, recebe o elemento [x] e a lista ordenada [l] como argumentos e retorna uma permutação ordenada da lista [x::l]: 
- *)
-
-Function binsert' x l {measure length l} :=
-  match l with
-  | [] => [x]
-  | [y] => if (x <=? y)
-           then [x; y]
-           else [y; x]
-  | h1::h2::tl =>
-      let len := length l in
-      let mid := len / 2 in
-      let l1 := firstn mid l in
-      let l2 := skipn mid l in
-      match l2 with
-      | [] => l
-      | h2'::l2' => if x =? h2'
-                    then l1 ++ (x ::l2)
-                    else
-                      if x <? h2'
-                      then binsert' x l1
-                      else binsert' x l2
-      end
-  end.
-Proof.
-  - intros. rewrite firstn_length_le.
-    + simpl length. apply Nat.div_lt; lia.
-    + simpl length. apply Nat.lt_le_incl. apply Nat.div_lt; lia.
-  - intros. rewrite <- teq1. rewrite skipn_length. apply Nat.sub_lt.
-    + simpl length. apply Nat.lt_le_incl. apply Nat.div_lt; lia.
-    + simpl length. apply Nat.div_str_pos. lia.
-Defined.
-
-Lemma teste0: (binsert' 2 [1;2;3]) = [1;2;2;3].
-Proof.
-  rewrite binsert'_equation. simpl. reflexivity.
-Qed.
-
-(**
-As funções [binsert] e [binsert'] representam o mesmo algoritmo:
- *)
-
-Lemma binsert_equiv_binsert': forall l x, binsert x l = binsert' x l.
-Proof.
-Admitted.
-
-(**
- E portanto a correção de [binsert'] é imediata:
-*)
-
-Corollary binsert'_correct: forall l x, Sorted le l -> Sorted le (binsert' x l).
-Proof.
-  intros l x H. rewrite <- binsert_equiv_binsert'. apply binsert_correct. assumption.
-Qed.
-
-(**
-O algoritmo principal é dado a seguir:
- *)
-
-Fixpoint binsertion_sort (l: list nat) :=
-  match l with
-  | [] => []
-  | h::tl => binsert h (binsertion_sort tl)
-  end.
-
-(* begin hide *)
+Require Import Arith List Recdef.
+Require Import Coq.Program.Wf.
 Require Import Permutation.
 (* end hide *)
 
-(**
-   O teorema abaixo é o resultado principal a ser provado. Observe que pode ser conveniente dividir esta prova em outras provas menores. Isto significa que a formalização pode ficar mais simples e mais organizada com a inclusão de novos lemas.
- *)
 
-Theorem binsertion_sort_correct: forall l, Sorted le (binsertion_sort l) /\ Permutation l (binsertion_sort l).
+
+Inductive sorted :list nat -> Prop :=
+  | nil_sorted : sorted nil
+  | one_sorted: forall n:nat, sorted (n :: nil)
+  | all_sorted : forall (x y: nat) (l:list nat), sorted (y :: l) -> x <= y -> sorted (x :: y :: l).
+
+
+
+Definition le_all x l := forall y, In y l -> x <= y.
+
+(* begin hide *)
+Infix "<=*" := le_all (at level 70, no associativity).
+(* end hide *)
+
+
+Lemma tail_sorted: forall l a, sorted (a :: l) -> sorted l.
+(* begin hide *)
+Proof.
+  Admitted.
+
+
+(* end hide *)
+
+
+Lemma le_all_sorted: forall l a, a <=* l -> sorted l -> sorted (a :: l).
+Proof.
+  Admitted.
+      
+
+Lemma remove_sorted: forall l a1 a2, sorted (a1 :: a2 :: l) -> sorted (a1 :: l).
+(* begin hide *)
+Proof.
+  Admitted.
+(* end hide *)
+
+
+Lemma sorted_le_all: forall l a, sorted(a :: l) -> a <=* l.
+Proof.
+  Admitted.
+
+
+Fixpoint num_oc n l  :=
+  match l with
+    | nil => 0
+    | h :: tl =>
+      if n =? h then S(num_oc n tl) else  num_oc n tl
+  end.
+
+
+Definition perm l l' := forall n:nat, num_oc n l = num_oc n l'.
+
+
+Lemma perm_refl: forall l, perm l l.
+(* begin hide *)
+Proof.
+Admitted.
+(* end hide *)
+
+
+Lemma num_oc_append: forall n l1 l2, num_oc n l1 + num_oc n l2 = num_oc n (l1 ++ l2).
+(* begin hide *)
+Proof.
+  Admitted.
+(* end hide *)
+
+
+Definition sorted_pair_lst (p: list nat * list nat) :=
+sorted (fst p) /\ sorted (snd p).
+
+
+Definition len (p: list nat * list nat) :=
+   length (fst p) + length (snd p).
+
+(* printing *)
+
+Function merge (p: list nat * list nat) {measure len p} :=
+match p with
+  | (nil, l2) => l2
+  | (l1, nil) => l1
+  | ((hd1 :: tl1) as l1, (hd2 :: tl2) as l2) =>
+if hd1 <=? hd2 then hd1 :: merge (tl1, l2)
+      else hd2 :: merge (l1, tl2)
+   end.
+
+(* begin hide *)
+Proof.
+  Admitted.
+(* end hide *)
+
+
+Lemma merge_in: forall y p, In y (merge p) -> In y (fst p) \/ In y (snd p).
+(* begin hide *)
+Proof.
+Admitted.
+(* end hide *)
+
+
+Theorem merge_sorts: forall p, sorted_pair_lst p -> sorted (merge p).
+(* begin hide *)
+Proof.
+  Admitted.
+
+(* end hide *)
+
+
+Function mergesort (l: list nat) {measure length l}:=
+  match l with
+  | nil => nil
+  | hd :: nil => l
+  | hd :: tail =>
+     let n := length(l) / 2 in
+     let l1 := firstn n l in
+     let l2 := skipn n l in
+     let sorted_l1 := mergesort(l1) in
+     let sorted_l2 := mergesort(l2) in
+     merge (sorted_l1, sorted_l2)
+  end.
+
+(* begin hide *)
+Proof.
+Admitted.
+(* end hide *)
+
+
+Theorem mergesort_sorts: forall l, sorted (mergesort l).
+Proof.
+  Admitted.
+
+
+Lemma merge_num_oc: forall n p, num_oc n (merge p) = num_oc n (fst p) + num_oc n (snd p).
+(* begin hide *)
+Proof.
+Admitted.
+(* end hide *)
+
+
+Theorem mergesort_is_perm: forall l, perm l (mergesort l).
+Proof.
+  Admitted.
+
+
+Theorem mergesort_is_correct: forall l, perm l (mergesort l) /\ sorted (mergesort l).
 Proof.
   Admitted.
